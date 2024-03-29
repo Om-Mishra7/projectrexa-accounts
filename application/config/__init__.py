d02f3778d89281809aceab66827f0b14cd69fe58
Om Mishra <contact@om-mishra.com>
2024-03-29 18:07:26 +0530

    Version Bump

--- application/config/__init__.py
+++ application/config/__init__.py
@@ -11,7 +11,7 @@ class Config:
 
         self.application_info = {
             'name': 'ProjectRexa Single Sign-On Service',
-            'version': '3.0.1',
+            'version': '3.1.0',
             'description': 'Single Sign-On service for ProjectRexa and its services',
         }
 
